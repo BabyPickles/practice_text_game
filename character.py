ed31ff5df37a55cd7e4c3d8306cb2c0ec09cba10
Jonathan Svensson <jonathan-svensson@hotmail.com>
2022-10-18 17:41:02 +0200

    Adding character class selection

--- character.py
+++ character.py
@@ -18,19 +18,16 @@ def char_class():
         print("Options: Warrior, Hunter, Mage")
         userInput = input()
         if userInput == "Warrior":
-            char_class = Warrior
+            char_class = userInput
             print("Very well, a " + char_class + " it is.")
         elif userInput == "Hunter":
-            char_class = Hunter
+            char_class = userInput
             print("Very well, a " + char_class + " it is.")
         elif userInput == "Mage":
-            char_class = Mage
+            char_class = userInput
             print("Very well, a " + char_class + " it is.")
         else:
             print("\nInvalid class, please choose one of the options.")
 
-# Character Gender
-#def char_gender():
-
-# Character Class
-#def char_class():
+# Character Stats
+#def char_stats():
